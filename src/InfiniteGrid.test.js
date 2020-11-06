927c85b73a2286a727ba0c7e5753e30747c68d1d
Steve Elmer <steve@summergrand.com>
2020-11-05 22:00:18 -0800

    Add InfiniteGrid scroll and click tests

--- src/InfiniteGrid.test.js
+++ src/InfiniteGrid.test.js
@@ -1,11 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import InfiniteGrid from 'InfiniteGrid'
 
 // Test failed without explict Provider wrapper, borrowed this from index.js
 test('renders InfiniteGrid', () => {
     let items = [];
     const nextPage = jest.fn();
-    const openItemHandler = jest.fn();
+    const openItem = jest.fn();
     const renderItem = (item) => {
         return (
             <div>Item {item.id}</div>
@@ -19,7 +20,7 @@ test('renders InfiniteGrid', () => {
             items={items}
             itemsPerRow="3"
             nextPage={nextPage}
-            openItem={openItemHandler}
+            openItem={openItem}
             renderItem={renderItem}
         />
     );
@@ -33,3 +34,59 @@ test('renders InfiniteGrid', () => {
     const gridElement = screen.getByText("Item 1");
     expect(gridElement).toBeInTheDocument();
 });
+
+test('calls nextPage handler on scrolling', () => {
+    let items = [];
+    const nextPage = jest.fn();
+    const openItem = jest.fn();
+    const renderItem = (item) => {
+        return (
+            <div height="200px">Item {item.id}</div>
+        )
+    }
+
+    for(var id=0; id<15; id++) { items.push({ id: id })}
+
+    render(
+        <InfiniteGrid
+            items={items}
+            itemsPerRow="3"
+            nextPage={nextPage}
+            openItem={openItem}
+            renderItem={renderItem}
+        />
+    );
+
+    expect(nextPage).toHaveBeenCalledTimes(0)
+    fireEvent.scroll(global, { target: { scrollY: 100 }})
+    expect(nextPage).toHaveBeenCalledTimes(1)
+});
+
+test('calls openItem when item is clicked', async () => {
+    let items = [];
+    const nextPage = jest.fn();
+    const openItem = jest.fn();
+    const renderItem = (item) => {
+        return (
+            <div height="200px">Item {item.id}</div>
+        )
+    }
+
+    for(var id=0; id<15; id++) { items.push({ id: id })}
+
+    render(
+        <InfiniteGrid
+            items={items}
+            itemsPerRow="3"
+            nextPage={nextPage}
+            openItem={openItem}
+            renderItem={renderItem}
+        />
+    );
+
+    expect(openItem).toHaveBeenCalledTimes(0)
+    const gridElement = screen.getByText("Item 1");
+    await userEvent.click(gridElement)
+    expect(openItem).toHaveBeenCalledTimes(1)
+});
+
